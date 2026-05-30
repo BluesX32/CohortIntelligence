c351f2493d995566c6b184fe7b0b5f04177a185f
BluesX32 <mxiong5@jh.edu>
2026-05-30 15:44:02 -0400

    Fix: error in connection

--- R/app.R
+++ R/app.R
@@ -10,15 +10,14 @@
 #' @param vocab_schema Vocabulary schema. Defaults to `cdm_schema`.
 #' @param cohort_table Name of the cohort table. Default `"cohort"`.
 #' @param cohort_definition_id Cohort definition ID. Default `1L`.
-#' @param ... Additional arguments forwarded to [shiny::runApp()].
 #' @export
-launch_cohort_intelligence <- function(connection_details   = NULL,
-                                        cdm_schema           = NULL,
-                                        cohort_schema        = NULL,
-                                        vocab_schema         = cdm_schema,
-                                        cohort_table         = "cohort",
-                                        cohort_definition_id = 1L,
-                                        ...) {
+launch_cohort_intelligence <- function(
+    connection_details   = NULL,
+    cdm_schema           = NULL,
+    cohort_schema        = NULL,
+    vocab_schema         = cdm_schema,
+    cohort_table         = "cohort",
+    cohort_definition_id = 1L) {
   if (!requireNamespace("shiny", quietly = TRUE)) {
     rlang::abort("Package 'shiny' is required to launch the dashboard.")
   }
@@ -35,7 +34,7 @@ launch_cohort_intelligence <- function(connection_details   = NULL,
   .cohort_intel_env$cohort_table         <- cohort_table
   .cohort_intel_env$cohort_definition_id <- as.integer(cohort_definition_id)
 
-  shiny::runApp(app_dir, ...)
+  shiny::runApp(app_dir)
 }
 
 .cohort_intel_env <- new.env(parent = emptyenv())
